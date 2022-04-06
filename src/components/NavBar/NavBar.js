a6246d7579a6e45c768855612bec9eb61621a236
Yago Velazquez <yagovelazquezsouzaa@gmail.com>
2022-04-06 02:55:28 +0100

    Feature: add navbar to the page

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,55 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import NavLink from "../commom/NavLink";
+
+import Button from "../commom/Button";
+
+function NavBar() {
+  const links = ["treatments", "staff", "calendar", "profile"];
+
+  return (
+    <Flex
+      align="center"
+      justify="space-between"
+      bg="black"
+      width="100%"
+      height="95px"
+      padding="12px 30px"
+      borderBottom="1px solid white"
+      pos="sticky" top="0" left="0"
+      zIndex={1}
+    >
+      <Flex align="center" justify="center" gap="35px">
+        <Link to="/">
+          <Image src="/treeLogo.jpg" alt="Logo" boxSize="71px" />
+        </Link>
+
+        {links.map((link, index) => {
+          if (index === 0) {
+            return (
+              <NavLink
+                key={link}
+                marginLeft="15px"
+                font="buttonLabel"
+                activelink="true"
+                to={`/${link}`}
+              >
+                {link}
+              </NavLink>
+            );
+          }
+
+          return (
+            <NavLink  activelink="true" key={link} font="buttonLabel" to={`/${link}`}>
+              {link}
+            </NavLink>
+          );
+        })}
+      </Flex>
+
+      <Button>Check Rates</Button>
+    </Flex>
+  );
+}
+
+export default NavBar;
