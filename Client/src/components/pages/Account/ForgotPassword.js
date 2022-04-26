2079f2e5d6ee21d75135a8f61cfa89162e5416c2
Yago Velazquez <yagovelazquezsouzaa@gmail.com>
2022-04-26 11:42:59 +0100

    Feature: client add forgot password page

--- Client/src/components/pages/Account/ForgotPassword.js
+++ Client/src/components/pages/Account/ForgotPassword.js
@@ -1,8 +1,17 @@
 import ForgotPasswordForm from "./ForgotPasswordForm";
+import { useParams } from "react-router-dom";
+import React from "react";
+import ChangePassForm from "./ChangePassForm";
 
 function ForgotPassword() {
+  const params = useParams();
 
-  return <ForgotPasswordForm></ForgotPasswordForm>;
+  return (
+    <React.Fragment>
+      {!params.token && <ForgotPasswordForm />}
+      {params.token && <ChangePassForm token={params.token} />}
+    </React.Fragment>
+  );
 }
 
 export default ForgotPassword;
--- Client/src/components/pages/Account/ForgotPasswordForm.js
+++ Client/src/components/pages/Account/ForgotPasswordForm.js
@@ -4,23 +4,30 @@ import { Link } from "react-router-dom";
 import React from "react";
 import { VStack } from "@chakra-ui/react";
 import * as Yup from "yup";
+import { useMutation } from "react-query";
+import { recoveryServerCall } from "../../../Lib/fetchServer";
+import { serverUrl } from "../../../ReactQuery/queryUrl";
+
 
 function ForgotPasswordForm() {
-  const inputContents = [
-    { label: "Email", name: "email", type: "email" },
+  const inputContents = [{ label: "Email", name: "email", type: "email" }];
 
-  ];
+  const { mutate, error, data: success} = useMutation((data) => {
+    return recoveryServerCall(data, data.url);
+  });
 
   const heading = "Recover Password";
   const subheading = "Please enter your email:";
-  const buttonLabel = "Create my Account";
+  const buttonLabel = "Recover";
 
   const validationSchema = Yup.object({
     email: Yup.string().required("Please enter a valid email address"),
   });
 
-  const submitFormHandler = (values) => {
-  }
+  const submitFormHandler = (email) => {
+    const url = `${serverUrl}/user/recover`;
+    mutate({ email, url });
+  };
 
   return (
     <React.Fragment>
@@ -33,8 +40,10 @@ function ForgotPasswordForm() {
           validationSchema={validationSchema}
           onSubmitForm={submitFormHandler}
           isValidatingOnchange={true}
+          successMessage={success?.message}
+          errorMessage={error?.message}
         ></Form>
-        );
+
         <Text variant="formText" fontWeight="300" marginTop="24px">
           Remember your password?{" "}
           {<Link to="/account/login">back to login</Link>}
