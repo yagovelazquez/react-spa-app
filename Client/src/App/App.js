dc7cd82beab4ae8b0089fae9609ad0f93495111b
Yago Velazquez <yagovelazquezsouzaa@gmail.com>
2022-04-26 11:33:29 +0100

    Feature: client add login, recovery, register, profile routes

--- Client/src/App/App.js
+++ Client/src/App/App.js
@@ -1,34 +1,65 @@
 import "../App.css";
 import NavBar from "../components/NavBar/NavBar";
-import React from "react";
+import React, { useRef } from "react";
 import Routes from "./Routes";
 import Footer from "../components/Footer/Footer";
-import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { queryClient } from "../ReactQuery/queryClient";
 import { useEffect } from "react";
 import useUser from "../components/Hooks/useUser";
+import { useLocation } from "react-router-dom";
+import LoadingModal from "../components/commom/LoadingModal";
+import { useIsMutating } from "react-query";
 
 function App() {
+  const { user, clearUser } = useUser();
 
-
+  const logoutUser = useRef();
 
   useEffect(() => {
-    queryClient.invalidateQueries('user')
-  }, [])
+    if (user) {
+
+    
+
+      const tokenExpirationTime =
+      (user.expiresIn - Math.floor(Date.now() / 1000)) * 1000;
+      logoutUser.current = setTimeout(clearUser, tokenExpirationTime);
+  
+    return () => {
+      clearTimeout(logoutUser.current);
+    };
+    }
 
 
- 
+    return () => {
+      clearTimeout(logoutUser.current);
+    };
+  }, [user]);
+
+  let location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  useEffect(() => {
+    queryClient.invalidateQueries("user", { exact: true });
+  }, []);
+
+  const isMutating = useIsMutating();
+  const isMutatingInterest = queryClient.isMutating({
+    mutationKey: "interests",
+  });
 
   return (
     <React.Fragment>
-      <QueryClientProvider client={queryClient}>
-        <NavBar></NavBar>
-        <Routes />
-
-        <Footer />
-        <ReactQueryDevtools></ReactQueryDevtools>
-      </QueryClientProvider>
+      <LoadingModal
+        isLoading={isMutating && !isMutatingInterest}
+      ></LoadingModal>
+      <NavBar></NavBar>
+      <Routes />
+      <Footer />
+      <ReactQueryDevtools></ReactQueryDevtools>
     </React.Fragment>
   );
 }
--- Client/src/App/Routes.js
+++ Client/src/App/Routes.js
@@ -3,17 +3,27 @@ import Home from "../components/pages/Home/Home";
 import Login from "../components/pages/Account/Login";
 import Register from './../components/pages/Account/Register';
 import ForgotPassword from "../components/pages/Account/ForgotPassword";
+import Profile from "../components/pages/Profile/Profile";
+import ProfileEdit from "../components/pages/Profile/Edit/ProfileEdit";
+import React from "react";
+import useUser from "../components/Hooks/useUser";
 
 
 
 function Routes() {
+
+  const {user} = useUser()
+
+
   return (
     <RoutesReact>
       <Route path="/" element={<Home />}></Route>
       <Route path="/account/login" element={<Login />}></Route>
       <Route path="/account/register" element={<Register />}></Route>
       <Route path="/account/forgot-password" element={<ForgotPassword />}></Route>
-      <Route path="/profile" element={<Navigate to="/account/login" />}></Route>
+      <Route path="/account/forgot-password/:token" element={<ForgotPassword />}></Route>
+      <Route path="/profile" element={user ? <Profile /> : <Navigate to="/account/login"></Navigate>}></Route>
+      <Route path="/profile/edit" element={user ? <ProfileEdit /> : <Navigate to="/account/login"></Navigate>}></Route>
     </RoutesReact>
   );
 }
